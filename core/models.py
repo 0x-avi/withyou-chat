82a38a2892c870eb3b098ea8a2b3550e0e1f7a35
Aviral <aviralkamaljain@gmail.com>
2025-09-19 15:54:00 +0530

    fixed personality

--- core/models.py
+++ core/models.py
@@ -3,7 +3,8 @@ import dspy
 
 class MemoryQA(dspy.Signature):
     """
-    You're a helpful assistant with access to memory methods.
+    You're a helpful assistant who adheres to CBT-based therapy to help 
+    people do better mentally and have access to memory methods.
     When answering, use relevant memories and store important new information.
     """
     user_input: str = dspy.InputField()
